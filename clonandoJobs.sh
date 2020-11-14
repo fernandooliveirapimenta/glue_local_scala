21fa1858c5796d02a41205a0b413d5c22b8f1a52
Fernando Pimenta <fosilva@brasilseg.com.br>
2020-11-14 16:50:59 -0300

    validando script

---
+++ clonandoJobs.sh
@@ -0,0 +1,130 @@
+
+#assistencia prd
+./glue.sh job-clone --job-name-source carga_assistencia_redis_ab_mobile_assistencia_db\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_assistencia_redis_ab_mobile_assistencia_db\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source carga_assistencia_redis_ab_mobile_assistencia_db_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_assistencia_redis_ab_mobile_assistencia_db_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source carga_assistencia_redis_abs_mobile_assistencia_db\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_assistencia_redis_abs_mobile_assistencia_db\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source carga_assistencia_redis_abs_mobile_assistencia_db_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_assistencia_redis_abs_mobile_assistencia_db_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+
+#proposta prd
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_home\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_home\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_prestamista_historico\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_prestamista_historico\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_prestamista_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_prestamista_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_residencial_historico\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_residencial_historico\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_residencial_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_residencial_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_rural_ab_historico\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_rural_ab_historico\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_rural_ab_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_rural_ab_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_rural_abs_historico\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_rural_abs_historico\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_rural_abs_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_rural_abs_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_vida_historico\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_vida_historico\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_vida_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_vida_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source bb30_carga_proposta_redis_preparacao\
+ --region-source us-east-1 --profile-source prd --job-name-target bb30_carga_proposta_redis_preparacao\
+  --region-target us-east-1 --profile-target pessoal
+
+aws iam create-policy --policy-name SSMGetParameterAccess --policy-document '{    "Version": "2012-10-17",    "Statement": [        {            "Sid": "VisualEditor0",            "Effect": "Allow",            "Action": [                "ssm:DescribeParameters",                "ssm:GetParameters",                "ssm:GetParameter"            ],            "Resource": "*"        }    ]}' --profile pessoal
+aws iam attach-role-policy --policy-arn arn:aws:iam::646025261134:policy/SSMGetParameterAccess --role-name Brasilseg-Glue-Role --profile pessoal
+
+# evento sinistro
+./glue.sh job-clone --job-name-source carga_evento_sinistro_redis_ab_mobile_seguros_db\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_evento_sinistro_redis_ab_mobile_seguros_db\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source carga_evento_sinistro_redis_ab_mobile_seguros_db_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_evento_sinistro_redis_ab_mobile_seguros_db_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source carga_evento_sinistro_redis_abs_mobile_seguros_db\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_evento_sinistro_redis_abs_mobile_seguros_db\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source carga_evento_sinistro_redis_abs_mobile_seguros_db_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_evento_sinistro_redis_abs_mobile_seguros_db_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+
+echo "Sinistro BB3.0"
+
+./glue.sh job-clone --job-name-source carga_sinistro_redis_abs_mobile_seguros_db\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_sinistro_redis_abs_mobile_seguros_db\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source carga_sinistro_redis_abs_mobile_seguros_db_incremental\
+ --region-source us-east-1 --profile-source prd --job-name-target carga_sinistro_redis_abs_mobile_seguros_db_incremental\
+  --region-target us-east-1 --profile-target pessoal
+
+
+echo "Aviso Web"
+
+./glue.sh job-clone --job-name-source asw_carga_redis_ab_seguros_db_incremental_async_proposta\
+ --region-source us-east-1 --profile-source prd --job-name-target asw_carga_redis_ab_seguros_db_incremental_async_proposta\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source asw_carga_redis_ab_seguros_db_incremental_async_sinistro\
+ --region-source us-east-1 --profile-source prd --job-name-target asw_carga_redis_ab_seguros_db_incremental_async_sinistro\
+  --region-target us-east-1 --profile-target pessoal
+
+./glue.sh job-clone --job-name-source asw_carga_redis_ab_seguros_db_historico_etl\
+ --region-source us-east-1 --profile-source prd --job-name-target asw_carga_redis_ab_seguros_db_historico_etl\
+  --region-target us-east-1 --profile-target pessoal
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ comparandoJobGluePessoal.sh
@@ -0,0 +1,36 @@
+aws glue get-job --job-name asw_carga_redis_ab_seguros_db_historico_etl \
+--region us-east-1 --profile prd --output json > dev.txt && \
+ aws glue get-job --job-name asw_carga_redis_ab_seguros_db_historico_etl \
+  --region us-east-1 --profile pessoal --output json > prd.txt && vimdiff dev.txt prd.txt
+
+
+
+aws glue get-job --job-name carga_sinistro_redis_abs_mobile_seguros_db \
+--region us-east-1 --profile prd --output json > dev.txt && \
+ aws glue get-job --job-name carga_sinistro_redis_abs_mobile_seguros_db \
+  --region us-east-1 --profile pessoal --output json > prd.txt && vimdiff dev.txt prd.txt
+
+python3.7 glue.py job-update-parameters --job-name carga_assistencia_redis_ab_mobile_assistencia_db --parameters "{'Command':{'ScriptLocation':'s3://datalake-glue-scripts/carga_historica/carga_redis/assistencia_db_carga_redis_unificada.scala'},'DefaultArguments':{'--extra-jars':'s3://datalake-glue-scripts/dependencias_scala/jedis-combined-1.0.jar,s3://datalake-glue-scripts/dependencias_scala/Assistencia_AB/entityDependencies.jar','--extra-files': 's3://datalake-glue-scripts/estrutura_tabela/estrutura_assistencia_ab_mobile.json,s3://datalake-glue-scripts/estrutura_query/AssistenciaABQuerys.json','--redis_host':'/brseg/elasticache/brasilseg-redis/endpoint','--redis_port':'/brseg/elasticache/brasilseg-redis/porta','--redis_ssl':'true','--redis_auth':'/brseg/elasticache/brasilseg-redis/token','--environment':'aws','--database': 'ab','--redshift_schema': 'spectrum_assistencia_db_ab_mobile','--schema': 'estrutura_assistencia_ab_mobile.json','--queries': 'AssistenciaABQuerys.json','--enable-spark-ui': 'true','--conf': 'spark.driver.maxResultSize=10g','--spark-event-logs-path': 's3://brasilseg-glue-log/carga_assistencia_redis_ab_mobile_assistencia_db/','--enable-s3-parquet-optimized-committer':'true'},'GlueVersion': '2.0'}" --region us-east-1 --profile $profile
+
+./glue.sh job-update-parameters --job-name carga_assistencia_redis_ab_mobile_assistencia_db --parameters "{'Command':{'ScriptLocation':'s3://datalake-glue-scripts/carga_historica/carga_redis/assistencia_db_carga_redis_unificada.scala'},'DefaultArguments':{'--extra-jars':'s3://datalake-glue-scripts/dependencias_scala/jedis-combined-1.0.jar,s3://datalake-glue-scripts/dependencias_scala/Assistencia_AB/entityDependencies.jar','--extra-files': 's3://datalake-glue-scripts/estrutura_tabela/estrutura_assistencia_ab_mobile.json,s3://datalake-glue-scripts/estrutura_query/AssistenciaABQuerys.json','--redis_host':'/brseg/elasticache/brasilseg-redis/endpoint','--redis_port':'/brseg/elasticache/brasilseg-redis/porta','--redis_ssl':'true','--redis_auth':'/brseg/elasticache/brasilseg-redis/token','--environment':'aws','--database': 'ab','--redshift_schema': 'spectrum_assistencia_db_ab_mobile','--schema': 'estrutura_assistencia_ab_mobile.json','--queries': 'AssistenciaABQuerys.json','--enable-spark-ui': 'true','--conf': 'spark.driver.maxResultSize=10g','--spark-event-logs-path': 's3://brasilseg-glue-log/carga_assistencia_redis_ab_mobile_assistencia_db/','--enable-s3-parquet-optimized-committer':'true'},'GlueVersion': '2.0'}" --region us-east-1 --profile pessoal
+
+carga_assistencia_redis_ab_mobile_assistencia_db
+carga_assistencia_redis_ab_mobile_assistencia_db_incremental
+carga_assistencia_redis_abs_mobile_assistencia_db
+carga_assistencia_redis_abs_mobile_assistencia_db_incremental
+bb30_carga_proposta_redis_residencial_incremental
+bb30_carga_proposta_redis_rural_ab_historico
+bb30_carga_proposta_redis_vida_incremental
+carga_evento_sinistro_redis_ab_mobile_seguros_db
+carga_evento_sinistro_redis_ab_mobile_seguros_db_incremental
+carga_evento_sinistro_redis_abs_mobile_seguros_db
+carga_evento_sinistro_redis_abs_mobile_seguros_db_incremental
+bb30_carga_sinistro_redis_ab_historico
+bb30_carga_sinistro_redis_ab_incremental
+carga_sinistro_redis_abs_mobile_seguros_db
+carga_sinistro_redis_abs_mobile_seguros_db_incremental
+asw_carga_redis_ab_seguros_db_incremental_async_proposta
+asw_carga_redis_ab_seguros_db_incremental_async_sinistro
+asw_carga_redis_ab_seguros_db_historico_etl
+
+
